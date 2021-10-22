1e438890c7e976d6a35a61dd1c341029dae6ba5c
Gustavo Ramirez <gartek@gmail.com>
2021-10-23 00:06:02 +0200

    Res

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import wifi
 import usocket
+import esp32
 
 print('Executing code')
 pd_host = '192.168.1.114'
 pd_port = 15000
 wifi.wifi_connect()
-pd = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+pd = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
 pd.connect((pd_host, pd_port))
+print('Connected to {}:{}'.format(pd_host, pd_port))
+while True:
+    pot_value = pot.read()
+    pd.send(pot_value.to_bytes(8,"big"))
+    utime.sleep_ms(200)
